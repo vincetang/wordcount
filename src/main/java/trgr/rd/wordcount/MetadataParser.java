c6f19aa109bcbde96a774dc9ddf2069c8399faaf
Vince Tang <vince.tang@thomsonreuter.com>
2016-08-12 16:20:45 -0400

    fixed formatting and indentation

--- src/main/java/trgr/rd/wordcount/MetadataParser.java
+++ src/main/java/trgr/rd/wordcount/MetadataParser.java
@@ -12,71 +12,73 @@ import java.util.regex.Pattern;
 class MetadataParser {
 
     // Regex patterns for text processing of files
-	private static final Pattern PATTERN_STOP_WORDS = Pattern.compile(getStopWordsRegex());
-	private static final Pattern PATTERN_PUNC = Pattern.compile("[^\\w\\s]");
-	private static final Pattern PATTERN_MULTI_SPACE = Pattern.compile("[\\s]{2,}");
+    private static final Pattern PATTERN_STOP_WORDS = Pattern.compile(getStopWordsRegex());
+    private static final Pattern PATTERN_PUNC = Pattern.compile("[^\\w\\s]");
+    private static final Pattern PATTERN_MULTI_SPACE = Pattern.compile("[\\s]{2,}");
 
 
-	/**
-	 * Reads a file and returns an object representing the file's metadata
-	 * @param fileNumber - the sequential number of the file
-	 * @param path - the file being parsed
-	 * @return a FileMetadata object
-	 */
-	FileMetadata parse(int fileNumber, Path path)
+    /**
+     * Reads a file and returns an object representing the file's metadata
+     *
+     * @param fileNumber - the sequential number of the file
+     * @param path       - the file being parsed
+     * @return a FileMetadata object
+     */
+    FileMetadata parse(int fileNumber, Path path)
     {
-		try (BufferedReader reader = new BufferedReader(Files.newBufferedReader(path, Charset.forName("UTF-8"))))
+        try (BufferedReader reader = new BufferedReader(Files.newBufferedReader(path, Charset.forName("UTF-8"))))
         {
 
-			String line;
+            String line;
 
-			FileMetadata fmd = new FileMetadata(path.getFileName().toString(), fileNumber);
-			ArrayList<String> words;
-			while ((line = reader.readLine()) != null)
+            FileMetadata fmd = new FileMetadata(path.getFileName().toString(), fileNumber);
+            ArrayList<String> words;
+            while ((line = reader.readLine()) != null)
             {
-				// Remove stop words and get an array of words
-				words = parseLine(line);
+                // Remove stop words and get an array of words
+                words = parseLine(line);
 
-				// add each word to the FileMetadata object
-				fmd.addWords(words);
-			}
+                // add each word to the FileMetadata object
+                fmd.addWords(words);
+            }
 
-			// Once done processing, return fmd object
-			return fmd;
-		}
-		catch (IOException  e)
+            // Once done processing, return fmd object
+            return fmd;
+        } catch (IOException e)
         {
-			e.printStackTrace();
-		}
-		return null;
-	}
-	
-	/**
-	 * Uses regex to parse line into an array of words
-	 * @param line - A string of a line in the file
-	 * @return An arraylist where each element is a word
-	 */
-	private ArrayList<String> parseLine(String line)
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Uses regex to parse line into an array of words
+     *
+     * @param line - A string of a line in the file
+     * @return An arraylist where each element is a word
+     */
+    private ArrayList<String> parseLine(String line)
     {
 
-		String result = PATTERN_STOP_WORDS.matcher(
-				PATTERN_MULTI_SPACE.matcher(PATTERN_PUNC.matcher(line.toLowerCase()).replaceAll(""))
-						.replaceAll("")).replaceAll("");
+        String result = PATTERN_STOP_WORDS.matcher(
+                PATTERN_MULTI_SPACE.matcher(PATTERN_PUNC.matcher(line.toLowerCase()).replaceAll(""))
+                        .replaceAll("")).replaceAll("");
 
-		ArrayList<String> wordList = new ArrayList<>(Arrays.asList(result.split(" ")));
-		
-		// Clean up null and space elements
-		wordList.removeAll(Collections.singleton(""));
-		wordList.removeAll(Collections.singleton(" "));
-		
-		return wordList;
-	}
+        ArrayList<String> wordList = new ArrayList<>(Arrays.asList(result.split(" ")));
+
+        // Clean up null and space elements
+        wordList.removeAll(Collections.singleton(""));
+        wordList.removeAll(Collections.singleton(" "));
+
+        return wordList;
+    }
 
     /**
      * Parses the file containing stop words, separated by new lines
+     *
      * @return a regex string to capture stop words in the file
      */
-	private static String getStopWordsRegex()
+    private static String getStopWordsRegex()
     {
         try
         {
@@ -94,20 +96,18 @@ class MetadataParser {
 
                 StringBuilder stopWordsRegex = new StringBuilder("(?i)\\b(");
                 wordSet.forEach(word -> stopWordsRegex.append(word).append("|"));
-                stopWordsRegex.replace(stopWordsRegex.length()-1, stopWordsRegex.length(), "");
+                stopWordsRegex.replace(stopWordsRegex.length() - 1, stopWordsRegex.length(), "");
                 stopWordsRegex.append(")\\b");
                 System.out.println(stopWordsRegex);
 
                 return stopWordsRegex.toString();
-            }
-            catch (IOException e)
+            } catch (IOException e)
             {
                 e.printStackTrace();
             }
         } catch (Exception e)
         {
             e.printStackTrace();
-
         }
 
         return "";
--- src/main/java/trgr/rd/wordcount/WordCountApp.java
+++ src/main/java/trgr/rd/wordcount/WordCountApp.java
@@ -9,102 +9,100 @@ import java.nio.file.Paths;
 import java.util.*;
 
 
-public class WordCountApp
-{
-	private static ArrayList<Path> paths;
-	static ArrayList<Map<String, Integer>> top25Counts;
-	private ArrayList<FileMetadata> fileMetadata;
+public class WordCountApp {
+    private static ArrayList<Path> paths;
+    static ArrayList<Map<String, Integer>> top25Counts;
+    private ArrayList<FileMetadata> fileMetadata;
 
     /**
      * Constructor for WordCountApp
      */
-	private WordCountApp()
-	{
-		this.fileMetadata = new ArrayList<>();
-		top25Counts = new ArrayList<>();
-	}
-	
-	/**
-	 * Processes files' metadata and adds to fileMetadat ArrayList
-	 */
-	private void process()
-	{
-		MetadataParser parser = new MetadataParser();
+    private WordCountApp()
+    {
+        this.fileMetadata = new ArrayList<>();
+        top25Counts = new ArrayList<>();
+    }
+
+    /**
+     * Processes files' metadata and adds to fileMetadat ArrayList
+     */
+    private void process()
+    {
+        MetadataParser parser = new MetadataParser();
         for (int i = 0; i < paths.size(); i++)
         {
-        	this.fileMetadata.add(parser.parse(i+1, paths.get(i)));
+            this.fileMetadata.add(parser.parse(i + 1, paths.get(i)));
         }
-	}
-		
-	/**
-	 * Word counting application.
-	 */
-	    public static void main(String[] args)
-	    {
-	        if (args.length != 1)
-	        {
-	            //Require 1 argument specify the directory uto use
-	            System.err.println("Usage: " + WordCountApp.class.getCanonicalName() + " [directory-of-files]");
-	            System.exit(1);
-	        }
+    }
 
-	        Path dir = Paths.get(args[0]);
-	        if (!Files.exists(dir) || !Files.isDirectory(dir))
-	        {
-	            System.err.println("Directory '" + dir.toAbsolutePath() + "' does not exist, or is not a directory.");
-	            System.exit(2);
-	        }
+    /**
+     * Word counting application.
+     */
+    public static void main(String[] args)
+    {
+        if (args.length != 1)
+        {
+            //Require 1 argument specify the directory uto use
+            System.err.println("Usage: " + WordCountApp.class.getCanonicalName() + " [directory-of-files]");
+            System.exit(1);
+        }
 
-	        System.out.println("Using directory '" + dir.toAbsolutePath() + "'");
+        Path dir = Paths.get(args[0]);
+        if (!Files.exists(dir) || !Files.isDirectory(dir))
+        {
+            System.err.println("Directory '" + dir.toAbsolutePath() + "' does not exist, or is not a directory.");
+            System.exit(2);
+        }
 
-	        // Start application logic..
-	        System.out.println("Word count report...\n");
-	        
-	        // Create an array to store Path objects representing files to read
-			paths = new ArrayList<>();
+        System.out.println("Using directory '" + dir.toAbsolutePath() + "'");
 
-	        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir))
-			{
-	        	stream.forEach(paths::add);
-	        }
-	        catch (IOException | DirectoryIteratorException e)
-			{
-	        	// Exceptions getting file stream
-	        	e.printStackTrace();
-	        }
+        // Start application logic..
+        System.out.println("Word count report...\n");
 
-	        System.out.print(paths.toString());
+        // Create an array to store Path objects representing files to read
+        paths = new ArrayList<>();
 
-	        // parse each file and create metadata objects
-	        WordCountApp wca = new WordCountApp();
-	        wca.process();
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir))
+        {
+            stream.forEach(paths::add);
+        } catch (IOException | DirectoryIteratorException e)
+        {
+            // Exceptions getting file stream
+            e.printStackTrace();
+        }
 
-	        // Report for each file
-	        for (FileMetadata f : wca.fileMetadata)
-	        {
-	        	System.out.println(f.toString());
-	        }
+        System.out.print(paths.toString());
 
-	        // Summary of all files
-	        Map<String, Integer> combinedMap = new HashMap<>();
-			top25Counts.forEach(combinedMap::putAll);
-	        Map<String, Integer> sortedCombinedMap = FileMetadata.sortByValue(combinedMap);
+        // parse each file and create metadata objects
+        WordCountApp wca = new WordCountApp();
+        wca.process();
 
-	        System.out.print("Most frequent words in " + dir.toAbsolutePath() + '\n');
+        // Report for each file
+        for (FileMetadata f : wca.fileMetadata)
+        {
+            System.out.println(f.toString());
+        }
 
-	        StringBuilder s = new StringBuilder();
-			Set keys = sortedCombinedMap.keySet();
-			Iterator iter = keys.iterator();
-			int i = 0;
-			while (iter.hasNext() && i < 25)
-			{
-				String word = (String) iter.next();
-				Integer count = sortedCombinedMap.get(word);
-				s.append(word).append(" (").append(count).append(")\n");
-				i++;
-			}
+        // Summary of all files
+        Map<String, Integer> combinedMap = new HashMap<>();
+        top25Counts.forEach(combinedMap::putAll);
+        Map<String, Integer> sortedCombinedMap = FileMetadata.sortByValue(combinedMap);
 
-			System.out.println(s);
-	    }
+        System.out.print("Most frequent words in " + dir.toAbsolutePath() + '\n');
+
+        StringBuilder s = new StringBuilder();
+        Set keys = sortedCombinedMap.keySet();
+        Iterator iter = keys.iterator();
+        int i = 0;
+        while (iter.hasNext() && i < 25)
+        {
+            String word = (String) iter.next();
+            Integer count = sortedCombinedMap.get(word);
+            s.append(word).append(" (").append(count).append(")\n");
+            i++;
+        }
+
+        System.out.println(s);
+    }
 
 }
